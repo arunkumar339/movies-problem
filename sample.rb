199e7e8fc6666d9c666a2a849473eec836561429
arunkumar339 <arunkumar339@gmail.com>
2012-07-25 20:58:10 +0530

    made genres printing alphabetical

--- sample.rb
+++ sample.rb
@@ -1,11 +1,9 @@
 require 'rubygems'
 require 'json'
 #read json
-
 raw_json = IO.read('data/movies.json')
 #parse json
 parsed_json=JSON.parse(raw_json) 
-
 genres = []
 #extract genres
 parsed_json["movies"].each do |movie|
@@ -13,14 +11,15 @@ parsed_json["movies"].each do |movie|
 	genres<< genre unless genres.include? genre
 	end
 end
+genres=genres.sort
 #output the result
 genres.each do |genre|
 	total_rating=0.0
 	total_movies=0 
 	parsed_json["movies"].each do |movie|
 	if movie["genres"].include?genre
-		total_rating=total_rating+movie["rating"].to_f
-		total_movies=total_movies+1
+		total_rating+=movie["rating"].to_f
+		total_movies+=1
 	end
 
 	end
